b0d80e7043e538abf91040123c7ea38be6df1e21
arther606 <>
2022-03-19 17:16:04 +0800

    略微修改

--- function/array-to-unique.js
+++ function/array-to-unique.js
@@ -1,8 +1,17 @@
+/**
+ * 1. JavaScript专题之数组去重：https://segmentfault.com/a/1190000009867515
+ * 2.也谈JavaScript数组去重 https://www.toobug.net/article/array_unique_in_javascript.html
+ * 3.JavaScript删除数组重复元素的5个高效算法：https://tingbuqing.com/javascript-delete-array.html
+ *
+ * https://github.com/mqyqingfeng/Blog/issues/27
+ */
+
+
 console.log("JavaScript查找重复数据 ")
 
 var array = [1, 1, '1', '1'];
 
-function unique(array) {
+function arrayToUnique(array) {
     // res用来存储结果
     var res = [];
     for (var i = 0,
@@ -21,4 +30,4 @@ function unique(array) {
     return res;
 }
 
-console.log(unique(array)); // [1, "1"]
+console.log(arrayToUnique(array)); // [1, "1"]
