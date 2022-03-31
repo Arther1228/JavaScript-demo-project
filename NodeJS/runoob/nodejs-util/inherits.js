61b03f9334d904706b6b8e3fedf2af4b5788b4ea
arther606 <>
2022-03-31 11:14:47 +0800

    update

---
+++ NodeJS/runoob/nodejs-util/inherits.js
@@ -0,0 +1,30 @@
+/** inhertis:继承;
+ *  util.inherits(constructor, superConstructor) 是一个实现对象间原型继承的函数。 */
+
+var util = require('util');
+function Base() {
+    this.name = 'base';
+    this.base = 1991;
+    this.sayHello = function () {
+        console.log('Hello ' + this.name);
+    };
+}
+Base.prototype.showName = function () {
+    console.log(this.name);
+};
+function Sub() {
+    this.name = 'sub';
+}
+
+util.inherits(Sub, Base);
+
+var objBase = new Base();
+objBase.showName();
+objBase.sayHello();
+console.log(objBase);
+
+
+var objSub = new Sub();
+objSub.showName();
+//objSub.sayHello(); 
+console.log(objSub); 
